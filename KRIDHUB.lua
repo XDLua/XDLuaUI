d6d7e84992245355b7fc776332286b69707e956c
XDLua <noncoconarak@gmail.com>
2025-03-22 07:48:26 +0700

    Update KRIDHUB.lua

--- KRIDHUB.lua
+++ KRIDHUB.lua
@@ -4,17 +4,12 @@ local XDLuaUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/XDLua
 -- สร้างหน้าต่าง
 local window = XDLuaUI:CreateWindow("🔹 KRIDHUB 🔹")
 
--- เพิ่มแท็บ
+-- home
 local homeTab = window:AddTab("🏠 หน้าหลัก")
 window:AddTabDescription(homeTab, "🏠 หน้าหลัก")
--- เพิ่มคำอธิบายและเครดิต
 window:AddDescription(homeTab, "ยินดีต้อนรับสู่ KRIDHUB\nสคริปต์นี้คือสคริปต์รวมแมพ", "พัฒนาโดย: KRIDHUB")
--- เพิ่มปุ่มคัดลอกลิงค์ YouTube
 window:Youtube(homeTab, "https://www.youtube.com/@IronFang2008")
--- เพิ่มปุ่มคัดลอกลิงค์ดิสคอร์ด
 window:Discord(homeTab)
--- ฟอนต์
-local font = Enum.Font.GothamBlack
 -- FORSAKEN
 window:AddMapTab(
     "FORSAKEN",
